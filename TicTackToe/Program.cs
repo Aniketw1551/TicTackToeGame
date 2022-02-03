0d47c1abcf68d2bb7326ad98ad8df8836bbe4c36
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-02-03 10:31:57 +0530

    [Aniket] Adding Creating tictactoe board program

---
+++ TicTackToe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTackToe
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTackToe program");
+            TicTackToe ticTackToe = new TicTackToe();
+            ticTackToe.Board();
+        }
+    }
+}
